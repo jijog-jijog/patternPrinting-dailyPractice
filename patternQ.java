c8d3fa05a399171f0169e382c604f44cbb6cce92
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-26 11:22:27 +0530

    Fix loop condition and update star printing logic in patternQ class

--- patternQ.java
+++ patternQ.java
@@ -2,7 +2,7 @@ public class patternQ {
     public static void main(String[] args) {
         int n = 5;
 
-        for(int i=0; i<n; i++){
+        for(int i=0; i<=n; i++){
             {
                 for(int spc=0; spc<(n-i); spc++){
                     System.out.print(" ");
@@ -10,7 +10,7 @@ public class patternQ {
                 }
     
                 for(int col=0; col<=((i+(i-1))); col++){
-                    if(col==(n-i)+1 ||  col==(n+i)-3 || col==n) {
+                    if(col==1 ||  col==(i+(i-1)) || col==n) {
                          System.out.print("*");
                     }else{
                         System.out.print(" ");
