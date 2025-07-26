37d5337116ced866854308b386a3ae270b81aba2
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-26 10:29:59 +0530

    Refactor patternQ class to improve structure and readability

--- patternQ.java
+++ patternQ.java
@@ -1,4 +1,23 @@
 public class patternQ {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for(int i=0; i<n; i++){
+            {
+                for(int spc=0; spc<(n-i); spc++){
+                    System.out.print(" ");
+
+                }
     
+                for(int col=0; col<=((i+(i-1))); col++){
+                    String colval = col<(n-i)+1 && col<(n+i)-3 ? "*" : " ";
+                    System.out.print(colval);
+                }
+            }
+            System.out.println();
+        }
+
+    }
+
     
 }
