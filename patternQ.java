80dc896e9cca0e2e3c82a4999ff262cc445ad12d
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-28 23:36:23 +0530

    t

--- patternQ.java
+++ patternQ.java
@@ -1,7 +1,6 @@
 public class patternQ {
     public static void main(String[] args) {
         int n = 5;
-
         for(int i=0; i<=n; i++){
             {
                 for(int spc=0; spc<(n-i); spc++){
